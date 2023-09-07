8d188d645ca531c07b2c5c73268e8ef89ab3ba09
Christian Schuering <Saregadon@gmail.com>
2023-09-07 15:53:05 -0500

    Basic Environment for cartpole

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 #import dependencies
 import os
-import gym
+import gymnasium as gym
 import pytorch
 import stable_baselines3
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 
-env = gym.make('CartPole-v1')
+env_name = 'CartPole-v0' #maps to preinstall environment from gym
+env = gym.make(env_name) #creating environment
 
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10000)
+episodes = 5
+for episode in range(1, episodes+1):
+    state = env.reset()
+    done = False
+    score = 0
 
+    while not done:
+        env.render()
+        action = env.action_space.sample()
+        n_state, reward, done, info = env.step(action)
+        score += reward
+    print('Episode:{} Score:{}'.format(episode, score))
+env.close()

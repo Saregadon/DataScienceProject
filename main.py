bc44e9049bb856f8d2d8a77c1f424617246f1d39
Christian Schuering <Saregadon@gmail.com>
2023-09-07 17:35:38 -0500

    Trains and Saves PPO Model for Cartpole

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 import gymnasium as gym
 import stable_baselines3
 import tensorflow as tf
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO #Proximal Policy Optimization Algorithm
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 
@@ -13,6 +13,7 @@ env_name = 'CartPole-v1' #maps to preinstalled environment from gym
 env = gym.make(env_name) #creating environment
 observation, info = env.reset(seed=42)
 
+"""
 episodes = 5 #number of loops through the game
 for episode in range(1, episodes+1):
     state = env.reset()
@@ -31,5 +32,15 @@ for episode in range(1, episodes+1):
         #print(env.reset())
     print('Episode: {} Score: {}'.format(episode, score))
 env.close()
+"""
 
+# Make directories
 log_path = os.path.join('Training', 'Logs') 
+
+env = gym.make(env_name)
+env = DummyVecEnv([lambda: env])
+model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=log_path) #MLP = Multilayer Perceptron Policy
+
+model.learn(total_timesteps = 20000) #trains the model
+
+PPO_Path = os.path.join('Training','Saved Models', 'PPO_Model_Cartpole') #saves place of the model
